e7dd96e924213577e6b4fce31b84d3daa43b55b3
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-11 17:16:19 +0200

    initial

---
+++ HackerRank.WarmUpChallenges.SalesByMatch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank.WarmUpChallenges.SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
