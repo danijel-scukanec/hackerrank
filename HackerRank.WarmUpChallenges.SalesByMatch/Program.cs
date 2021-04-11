e20827cbd969f23baca982bab028f377a63ef224
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-11 17:24:26 +0200

    SalesByMatch

--- HackerRank.WarmUpChallenges.SalesByMatch/Program.cs
+++ HackerRank.WarmUpChallenges.SalesByMatch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HackerRank.WarmUpChallenges.SalesByMatch
 {
@@ -7,6 +8,36 @@ namespace HackerRank.WarmUpChallenges.SalesByMatch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static int sockMerchant(int n, int[] ar)
+        {
+            if (n <= 1)
+            {
+                return 0;
+            }
+
+            var hashtable = new Dictionary<int, int>();
+            var counter = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                if (hashtable.ContainsKey(ar[i]))
+                {
+                    hashtable[ar[i]]++;
+
+                    if (hashtable[ar[i]] % 2 == 0)
+                    {
+                        counter++;
+                    }
+                }
+                else
+                {
+                    hashtable.Add(ar[i], 1);
+                }
+            }
+
+            return counter;
         }
     }
 }
