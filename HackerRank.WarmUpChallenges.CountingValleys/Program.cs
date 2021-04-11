31aea78901161ba0877b8222384e48bd538a50a2
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-11 17:38:21 +0200

    CountingValleys

---
+++ HackerRank.WarmUpChallenges.CountingValleys/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HackerRank.WarmUpChallenges.CountingValleys
+{
+    class Program
+    {
+        public static int countingValleys(int steps, string path)
+        {
+            var height = 0;
+            var numberOfValleys = 0;
+            for (var i = 0; i < path.Length; i++)
+            {
+                if(path[i] == 'U')
+                {
+                    height++;
+                }
+                else
+                {
+                    height--;
+
+                    if (height == -1)
+                    {
+                        numberOfValleys++;
+                    }
+                }
+            }
+
+            return numberOfValleys;
+        }
+
+        static void Print(string path, int result)
+        {
+            Console.WriteLine($"Path {path} has {result} valleys");
+        }
+
+        static void Main(string[] args)
+        {
+            var path1 = "DDUUUUDD";
+            var result1 = countingValleys(path1.Length, path1);
+            Print(path1, result1);
+
+            var path2 = "UDDDUDUU";
+            var result2 = countingValleys(path2.Length, path2);
+            Print(path2, result2);
+
+        }
+    }
+}
--- HackerRank.WarmUpChallenges.SalesByMatch/Program.cs
+++ HackerRank.WarmUpChallenges.SalesByMatch/Program.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace HackerRank.WarmUpChallenges.SalesByMatch
 {
     class Program
     {
         static void Main(string[] args)
+        {           
+            var ar1 = new int[] { 1, 2, 1, 2, 1, 3, 2 };
+            var result1 = sockMerchant(ar1.Length, ar1);
+            Print(ar1, result1);
+        }
+
+        static void Print(int[] ar, int result)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Result for array {string.Join(',', ar)} is {result}");
         }
 
         static int sockMerchant(int n, int[] ar)
