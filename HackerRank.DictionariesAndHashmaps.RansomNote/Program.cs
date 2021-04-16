94f6e25bdf1c0fe34c996b94d905e6d4343043b8
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-16 13:16:12 +0200

    TwoStrings

---
+++ HackerRank.DictionariesAndHashmaps.RansomNote/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.DictionariesAndHashmaps.RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var magazine1 = new string[] { "give", "me", "one", "grand", "today", "night" };
+            var note1 = new string[] { "give", "one", "grand", "today" };
+            checkMagazine(magazine1, note1);
+
+            var magazine2 = new string[] { "two", "times", "three", "is", "not", "four" };
+            var note2 = new string[] { "two", "times", "two", "is", "four" };
+            checkMagazine(magazine2, note2);
+        }
+
+        static void checkMagazine(string[] magazine, string[] note)
+        {
+            var hashtable = new Dictionary<string, int>();
+
+            foreach (var word in magazine)
+            {
+                if (hashtable.ContainsKey(word))
+                {
+                    hashtable[word]++;
+                }
+                else
+                {
+                    hashtable.Add(word, 1);
+                }
+            }
+
+            foreach (var word in note)
+            {
+                var wordExists = hashtable.TryGetValue(word, out int counter);
+
+                if (!wordExists || counter == 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }                
+
+                hashtable[word]--;
+            }
+
+            Console.WriteLine("Yes");
+        }
+    }
+}
---
+++ HackerRank.DictionariesAndHashmaps.TwoStrings/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.DictionariesAndHashmaps.TwoStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(twoStrings("hello", "world"));
+            Console.WriteLine(twoStrings("hi", "world"));
+        }
+
+        static string twoStrings(string s1, string s2)
+        {
+            var hashtable = new Dictionary<char, int>();
+            foreach (var character in s1)
+            {
+                if (!hashtable.ContainsKey(character))
+                {
+                    hashtable.Add(character, 0);
+                }
+            }
+
+            foreach (var character in s2)
+            {
+                if (hashtable.ContainsKey(character))
+                {
+                    return "YES";
+                }
+            }
+
+            return "NO";
+        }
+    }
+}
