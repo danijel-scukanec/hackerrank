d1209184c70fbd8913c8867f4ddf816cb84b2555
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-22 18:16:17 +0200

    FibonacciNumbers

---
+++ HackerRank.Recursion.FibonacciNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HackerRank.Recursion.FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fibonacci(5));
+            Console.WriteLine(fibonacci(3));
+        }
+
+        public static int fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
