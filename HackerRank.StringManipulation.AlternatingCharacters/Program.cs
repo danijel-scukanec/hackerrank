9e95aa39159939fa114707e858cc1082fe2fcecc
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-22 09:03:54 +0200

    AlternatingCharacters

---
+++ HackerRank.StringManipulation.AlternatingCharacters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HackerRank.StringManipulation.AlternatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(alternatingCharacters("AABAAB"));
+            Console.WriteLine(alternatingCharacters("AAAA"));
+            Console.WriteLine(alternatingCharacters("BBBBB"));
+            Console.WriteLine(alternatingCharacters("ABABABAB"));
+            Console.WriteLine(alternatingCharacters("BABABA"));
+            Console.WriteLine(alternatingCharacters("AAABBB"));
+        }
+
+        static int alternatingCharacters(string s)
+        {
+            var min = 0;
+            for (var i = 1; i < s.Length; i++)
+            {
+                if(s[i] == s[i - 1])
+                {
+                    min++;
+                }                
+            }
+
+            return min;
+        }
+    }
+}
