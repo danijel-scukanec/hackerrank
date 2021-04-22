72c5bcca8a630ffa3e948dd15b2f6b427098dc2b
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-22 09:22:00 +0200

    MinimumAbsoluteDifferenceInAnArray

---
+++ HackerRank.GreedyAlgorithms.MinimumAbsoluteDifferenceInAnArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HackerRank.GreedyAlgorithms.MinimumAbsoluteDifferenceInAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(minimumAbsoluteDifference(new int[] { -2, 2, 4 }));
+            Console.WriteLine(minimumAbsoluteDifference(new int[] { 3, -7, 0 }));
+            Console.WriteLine(minimumAbsoluteDifference(new int[] { -59, -36, -13, 1, -53, -92, -2, -96, -54, 75 }));
+            Console.WriteLine(minimumAbsoluteDifference(new int[] { 1, -3, 71, 68, 17 }));
+        }
+
+        static int minimumAbsoluteDifference(int[] arr)
+        {
+            Array.Sort(arr);
+
+            var min = int.MaxValue;
+            for (var i = 1; i < arr.Length; i++)
+            {
+                var difference = Math.Abs(arr[i - 1] - arr[i]);
+                if (difference < min)
+                {
+                    min = difference;
+                    if (min == 0)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return min;
+        }
+    }
+}
