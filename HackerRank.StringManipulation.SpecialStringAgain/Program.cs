1b087831a6abf8d2c1c4daa0bd3289c2197f4b80
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-22 21:08:26 +0200

    SpecialStringAgain

--- HackerRank.StringManipulation.SpecialStringAgain/Program.cs
+++ HackerRank.StringManipulation.SpecialStringAgain/Program.cs
@@ -6,7 +6,62 @@ namespace HackerRank.StringManipulation.SpecialStringAgain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(substrCount(8, "mnonopoo"));
+            Console.WriteLine(substrCount(5, "asasd"));
+            Console.WriteLine(substrCount(7, "abcbaba"));
+            Console.WriteLine(substrCount(4, "aaaa"));
+            Console.WriteLine(substrCount(1, "a"));
+        }
+
+        static long substrCount(int n, string s)
+        {
+            var numberOfSpecialStrings = 0;
+            for (var i = 0; i < s.Length;)
+            {
+                var sequentialCounter = 1;
+                var sequentialIndex = i + 1;
+                while (sequentialIndex < n)
+                {
+                    if (s[i] == s[sequentialIndex])
+                    {
+                        sequentialCounter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    sequentialIndex++;
+                }
+
+                numberOfSpecialStrings += sequentialCounter * (sequentialCounter + 1) / 2;
+                i += sequentialCounter;
+            }
+
+            for (var i = 1; i < n; i++)
+            {
+                var sequentialIndex = 1;
+                while (i - sequentialIndex >= 0 && i + sequentialIndex < n)
+                {
+                    if (sequentialIndex > 1 && s[i - sequentialIndex] != s[i - sequentialIndex + 1])
+                    {
+                        break;
+                    }
+
+                    if (s[i] != s[i - sequentialIndex] && s[i - sequentialIndex] == s[i + sequentialIndex])
+                    {
+                        numberOfSpecialStrings++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                    sequentialIndex++;
+                }
+            }
+
+            return numberOfSpecialStrings;
         }
     }
 }
