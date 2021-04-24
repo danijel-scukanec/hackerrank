59d29fbb8b8c0078629c5b4dcd6926fdcc31a665
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-25 00:02:12 +0200

    BalancedBrackets

---
+++ HackerRank.StacksAndQueues.BalancedBrackets/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.StacksAndQueues.BalancedBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isBalanced("{"));
+            Console.WriteLine(isBalanced(")"));
+            Console.WriteLine(isBalanced(")))"));
+            Console.WriteLine(isBalanced("((("));
+            Console.WriteLine(isBalanced("{[()]}"));
+            Console.WriteLine(isBalanced("{[(])}"));
+            Console.WriteLine(isBalanced("{{[[(())]]}}"));
+        }
+
+        static string isBalanced(string s)
+        {
+            var stack = new Stack<char>();
+            foreach(var character in s)
+            {
+                if(character == '(' || character == '{' || character == '[')
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    if(stack.Count == 0)
+                    {
+                        return "NO";
+                    }
+
+                    var popedCharacter = stack.Pop();
+                    if(!(popedCharacter == '(' && character == ')' || popedCharacter == '{' && character == '}' || popedCharacter == '[' && character == ']'))
+                    {
+                        return "NO";
+                    }
+                }
+            }
+
+            return stack.Count == 0 ? "YES" : "NO";
+        }
+    }
+}
