dc7aa64603dd8c3f4cd6c138c6e14de9d668c41e
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-25 18:32:59 +0200

    Pairs

---
+++ HackerRank.Search.Pairs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Search.Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(pairs(1, new List<int> { 1, 2, 3, 4 }));
+            Console.WriteLine(pairs(2, new List<int> { 1, 5, 3, 4, 2 }));
+        }
+
+        public static int pairs(int k, List<int> arr)
+        {
+            var array = arr.ToArray();
+            var hashtable = new Dictionary<int, int>();
+            var numberOfPairs = 0;
+            for (var i = 0; i < array.Length; i++)
+            {
+                var numberToSearch1 = array[i] - k;
+                if(hashtable.ContainsKey(numberToSearch1))
+                {
+                    numberOfPairs++;
+                }
+
+                var numberToSearch2 = array[i] + k;
+                if (hashtable.ContainsKey(numberToSearch2))
+                {
+                    numberOfPairs++;
+                }
+                
+
+                hashtable.Add(array[i], 0);
+            }
+
+            return numberOfPairs;
+        }
+    }
+}
---
+++ HackerRank.Search.SwapNodes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRank.Search.SwapNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
