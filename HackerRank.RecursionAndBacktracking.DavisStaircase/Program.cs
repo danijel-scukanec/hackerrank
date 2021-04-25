9056253fa95af365dff53b5ce6cf7392759e486f
Danijel Scukanec <Danijel.Scukanec@iwgplc.com>
2021-04-26 00:04:42 +0200

    DavisStaircase

---
+++ HackerRank.RecursionAndBacktracking.DavisStaircase/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.RecursionAndBacktracking.DavisStaircase
+{
+    class Program
+    {
+        private static Dictionary<int, int> hashtable = new Dictionary<int, int>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(stepPerms(5));
+            Console.WriteLine(stepPerms(7));
+            Console.WriteLine(stepPerms(27));
+        }
+
+        static int stepPerms(int n)
+        {
+            Console.WriteLine($"Count for n={n}");
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            if (n == 2)
+            {
+                return 2;
+            }
+
+            if (n == 3)
+            {
+                return 4;
+            }
+
+            if (!hashtable.ContainsKey(n))
+            {
+                hashtable[n] = stepPerms(n - 1) + stepPerms(n - 2) + stepPerms(n - 3);
+            }
+
+            return hashtable[n];
+        }
+    }
+}
---
+++ HackerRank.Search.MinimumTimeRequired/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRank.Search.MinimumTimeRequired
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static long minTime(long[] machines, long goal)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
